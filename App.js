782379b629a63b35b1dcd21e1f014af9819d7b77
Kumari Surbhi <kumarisurbhi.ctps@gmailcom>
2025-01-12 19:12:12 +0530

    change

--- App.js
+++ App.js
@@ -1,7 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
+/**
+ * benefits of parcel
+ * HMR - hot model reloading- it reload automatically
+ * File Watcher Algorithm - which is written in C++ , Monitoring the file system for a given filepath
+ * Bundling
+ * Minify
+ * Clearing our code- by removing console log.
+ * Dev and Production Build
+ * Super fast building algorithm
+ * Image Optimization
+ * Catching While Development - a built-in feature that stores and reuses data to improve performance
+ * Compression
+ * compatble with older version of browser
+ * HTTPS on dev
+ * Manage PORT Number
+ * 
+ */
 
 // const heading = React.createElement("h1", {}, "Namaste react!");
 //         const root = ReactDOM.createRoot(document.getElementById("root"));
