5b7405a07d5dbba2cd224a3dcfdbdf8d4202f4f9
Kumari Surbhi <kumarisurbhi.ctps@gmail.com>
2025-07-02 21:01:57 +0530

    UI updated

--- src/components/Error.js
+++ src/components/Error.js
@@ -6,6 +6,7 @@ const Error =() =>{
     return (<div>
         <h2>{err.status + ":"+ err.statusText}</h2>
         <h3>{err.data}</h3>
+        <h4>please type correctly</h4>
     </div>);
 }
 export default Error;
